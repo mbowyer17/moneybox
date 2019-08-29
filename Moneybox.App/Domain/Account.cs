3f100f5608514a6f05ee9e83a6ac7c1617aaa59a
unknown <matthewbowyer17@gmail.com>
2019-08-29 15:11:10 +0100

    first commit

---
+++ Moneybox.App/Domain/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Moneybox.App
+{
+    public class Account
+    {
+        public const decimal PayInLimit = 4000m;
+        public const decimal WithdrawLimit = 0m;
+        public const decimal LowBalance = 500m;
+
+        public Guid Id { get; set; }
+
+        public User User { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public decimal Withdrawn { get; set; }
+
+        public decimal PaidIn { get; set; }
+
+
+        public void Withdraw(decimal amount)
+        {
+            // Checks if there is enough money to make a withdraw
+            if (!canWithdraw(amount))
+                throw new InvalidOperationException("Insufficient funds to make transfer");
+
+            // Calculates the Transaction
+            this.Balance = this.Balance - amount;
+            this.Withdrawn = this.Withdrawn - amount;
+        }
+
+        public bool canWithdraw(decimal amount)
+        {
+
+            var newBalance = this.Balance - amount;
+            return newBalance >= WithdrawLimit;
+        }
+
+        public bool BalanceLowAfterWithdraw (decimal amount)
+        {
+            var newBalance = this.Balance - amount;
+            return newBalance <= LowBalance;
+        }
+
+        public bool IsLowBalance()
+        {
+            return BalanceLowAfterWithdraw(0);
+        }
+    }
+}
---
+++ Moneybox.App/Features/WithdrawMoney.cs
@@ -0,0 +1,36 @@
+﻿using Moneybox.App.DataAccess;
+using Moneybox.App.Domain.Services;
+using System;
+
+namespace Moneybox.App.Features
+{
+    public class WithdrawMoney
+    {
+        private IAccountRepository accountRepository;
+        private INotificationService notificationService;
+
+        public WithdrawMoney(IAccountRepository accountRepository, INotificationService notificationService)
+        {
+            this.accountRepository = accountRepository;
+            this.notificationService = notificationService;
+        }
+
+        public void Execute(Guid fromAccountId, decimal amount)
+        {
+            // TODO:
+            var from = this.accountRepository.GetAccountById(fromAccountId);
+
+
+            // Calculate the Transaction
+            from.Withdraw(amount);
+
+            // Checked the balance
+            if (from.IsLowBalance())
+                this.notificationService.NotifyFundsLow(from.User.Email);
+
+            // Update the account 
+            this.accountRepository.Update(from);
+
+        }
+    }
+}
